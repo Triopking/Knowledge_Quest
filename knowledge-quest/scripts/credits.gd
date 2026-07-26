b94b0f2acca34393c97cf6664a42a7b56830c84a
Auguste Hemery <hemerau@whs.school.nz>
2026-07-27 09:39:55 +1200

    clean up
    credits, more writing, less deltas and cleanup

--- knowledge-quest/scripts/credits.gd
+++ knowledge-quest/scripts/credits.gd
@@ -11,13 +11,5 @@ func _process(_delta: float) -> void:
 	pass
 
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/labratory.tscn")
-
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
-
-
-func _on_credits_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+func _on_go_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
