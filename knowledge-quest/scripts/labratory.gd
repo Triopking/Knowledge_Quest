672a28d62dc16f7cb8db7c095b1942286720df5b
Auguste Hemery <hemerau@whs.school.nz>
2026-07-28 12:00:06 +1200

    Math level

--- knowledge-quest/scripts/labratory.gd
+++ knowledge-quest/scripts/labratory.gd
@@ -2,27 +2,28 @@ extends Node2D
 
 @onready var player: CharacterBody2D = $Player
 @onready var label: Label = $Area2D/Label
-
+var on
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	label.visible = false
+	on = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	
+	if on == true and Input.is_action_pressed("interact"):
+		get_tree().change_scene_to_file("res://scenes/math_level.tscn")
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == player:#when the player is on the door, it should go to the math level.
 		label.visible = true
-	elif body == player and Input.is_action_pressed("interact"):
-		get_tree().change_scene_to_file("res://scenes/math_level.tscn")
-	
-	
+		on = true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body == player:
 		label.visible = false
+		on = false
 	
