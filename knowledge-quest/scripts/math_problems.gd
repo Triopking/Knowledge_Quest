8bfc95c39ea2df9bb7b235d9e7b96da5c0cb9c78
Auguste Hemery <hemerau@whs.school.nz>
2026-07-31 11:59:05 +1200

    math problem

--- knowledge-quest/scripts/math_problems.gd
+++ knowledge-quest/scripts/math_problems.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+@onready var line_edit: LineEdit = $LineEdit
+@onready var label: Label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	line_edit.grab_focus()
+	line_edit.caret_column = (len(line_edit.text))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	label.text = ("Question: " Global.questionName)
